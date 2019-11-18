5ec3aad807c8489239954398f2c0718f6894485b
abdyek <yunusemrebulut123@gmail.com>
2019-11-18 13:11:26 +0300

    slider and menus kinda fixed

--- assets/slider.js
+++ assets/slider.js
@@ -1,3 +1,4 @@
+/*
 var sliders = document.querySelectorAll('input[type="range"]');
 var delayRange = sliders[0];
 var range = sliders[1];
@@ -18,3 +19,24 @@ var rangeValue = function () {
 
 delayRange.addEventListener("input", delayValue);
 range.addEventListener("input", rangeValue);
+*/
+var rangeSlider = document.getElementById("rs-range-line");
+var rangeBullet = document.getElementById("rs-bullet");
+
+var rangeSlider2 = document.getElementById("rs-range-line2");
+var rangeBullet2 = document.getElementById("rs-bullet2");
+
+rangeSlider.addEventListener("input", showSliderValue, false);
+rangeSlider2.addEventListener("input", showSliderValue2, false);
+
+function showSliderValue() {
+  rangeBullet.innerHTML = rangeSlider.value;
+  var bulletPosition = (rangeSlider.value /rangeSlider.max);
+  rangeBullet.style.left = (bulletPosition * 578)/3 + "px";
+}
+
+function showSliderValue2() {
+  rangeBullet2.innerHTML = rangeSlider2.value;
+  var bulletPosition = (rangeSlider2.value /rangeSlider2.max);
+  rangeBullet2.style.left = (bulletPosition * 578)/3 + "px";
+}
